fbb3a7ba4d3d6b3785807c6c52c64ff56844891a
TimeToNowhere322 <fullmaster1999@gmail.com>
2024-10-04 13:24:47 +0300

    Add files via upload

---
+++ LAB_1/LAB_1_LVL_3_TUSK_8_FIX.py
@@ -0,0 +1,16 @@
+from math import *
+x = 0.05
+
+while x < 0.4:
+    x += 0.05
+    y = e**(2 * x)
+    i = 0
+    z = 1
+    s = 0
+    while abs(z) > 0.0001:
+        z = ((2 * x) ** i) / factorial(i)
+        i += 1
+        s += z
+
+    print(round(x,2),round(y, 4),round(s, 4))
+print('X   ', 'Y     ', 'Сумма выражения')
