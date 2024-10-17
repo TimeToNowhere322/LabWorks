9342d4f91390696ef3e393cb2515d9fe1978eed3
TimeToNowhere322 <fullmaster1999@gmail.com>
2024-10-17 12:59:35 +0300

    Update LAB_3_LVL_2_TUSK_1.py

--- LAB_3/LAB_3_LVL_2_TUSK_1.py
+++ LAB_3/LAB_3_LVL_2_TUSK_1.py
@@ -1,12 +1,10 @@
 A = []
-B = []
 a = 0
 while a != '':
     a = input()
     if a != '':
         A.append(int(a))
     else:
-        B.append(a)
         break
 z = min(A)
 print('Удвоенное минимальное значение',z * 2)
