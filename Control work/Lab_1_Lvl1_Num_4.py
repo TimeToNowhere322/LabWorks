a7ce7ebb79dd60a0b7125551fcc7dfe23549f7c7
TimeToNowhere322 <fullmaster1999@gmail.com>
2024-10-18 17:55:04 +0300

    Update Lab_1_Lvl1_Num_4.py

--- Control work/Lab_1_Lvl1_Num_4.py
+++ Control work/Lab_1_Lvl1_Num_4.py
@@ -1,6 +1,6 @@
 from  math import  *
 sum = 0
-x = int(input())
+x = float(input())
 for i in range (1, 10):
     sum += cos(i*x) / (x**(i-1))
 print("Сумма членов прогрессии при х =",x,"Равна",sum)
